9430c9b31e156f48dc8fff42ea2d7d217f3ba016
Ikrom771 <ikrom.study@gmail.com>
2025-06-09 16:30:49 +0500

    Update Hw_1.py

--- lesson-1/homework/Hw_1.py
+++ lesson-1/homework/Hw_1.py
@@ -1,1 +1,40 @@
+#1task. Perimetr and area of square
+a=5
+
+s=pow(a,2)
+p=4*a
+print('Area:', s)
+print('Perimetr:', p)
+
+
+#2task.Given diameter of circle. Find its length.
+d=3
+p=3.14
+l=p*d
+print('Length:', l)
+
+#3.Given two numbers a and b. Find their mean.
+
+a=10
+b=26
+m=(a+b)/2
+
+
+print('Mean: ',m)
+
+#4.Given two numbers a and b. Find their sum, product and square of each number.
+
+a = 5
+b = 3
+
+sum = a + b
+product = a * b
+sqr_a = a ** 2
+sqr_b = b ** 2
+
+# Output
+print("Sum:", sum)
+print("Product:", product)
+print("Square of a:", sqr_a)
+print("Square of b:", sqr_b)
 
